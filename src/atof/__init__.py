7f1d76883363093339956b358296b38129f58ada
Chris Hotza <chrishotzaoficial@gmail.com>
2026-09-21 01:27:33 -0300

    Expose routing API and bump release version

--- src/atof/__init__.py
+++ src/atof/__init__.py
@@ -4,10 +4,25 @@ from .partition import balance_error, edge_cut, initialize_balanced_partition, w
 from .strategies import BLOCReloc, PartitionResult
 from .topology import TopologyProfiler, TopologyProfile
 from .selector import HeuristicRegimeSelector, StrategyRecommendation
+from .routing import (
+    LearnedTopologyRouter,
+    RoutingEvaluation,
+    evaluate_holdout_predictions,
+    global_strategy_oracle,
+    graph_oracle,
+    routing_summary,
+    topology_vector,
+)
 
 __all__ = [
-    "BLOCReloc", "PartitionResult", "TopologyProfiler", "TopologyProfile",
-    "HeuristicRegimeSelector", "StrategyRecommendation", "balance_error",
-    "edge_cut", "initialize_balanced_partition", "weighted_cut",
+    "BLOCReloc", "PartitionResult",
+    "TopologyProfiler", "TopologyProfile",
+    "HeuristicRegimeSelector", "StrategyRecommendation",
+    "LearnedTopologyRouter", "RoutingEvaluation",
+    "evaluate_holdout_predictions", "global_strategy_oracle",
+    "graph_oracle", "routing_summary", "topology_vector",
+    "balance_error", "edge_cut", "initialize_balanced_partition",
+    "weighted_cut",
 ]
-__version__ = "0.1.0"
+
+__version__ = "0.2.0"
