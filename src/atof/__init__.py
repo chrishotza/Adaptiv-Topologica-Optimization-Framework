73f02f23a03285d3b00c1f82774a84e77900096e
Chris Hotza <chrishotzaoficial@gmail.com>
2026-09-21 02:36:19 -0300

    Export transfer diagnostics

--- src/atof/__init__.py
+++ src/atof/__init__.py
@@ -14,7 +14,11 @@ from .routing import (
     routing_summary,
     topology_vector,
 )
-from .generalization import summarize_generalization, summarize_routing_folds
+from .generalization import (
+    summarize_generalization,
+    summarize_routing_folds,
+    summarize_transfer_folds,
+)
 from .datasets import GraphDataset, standard_reference_corpus
 from .statistics import (
     bootstrap_mean_ci,
@@ -30,7 +34,7 @@ __all__ = [
     "LearnedTopologyRouter", "RoutingEvaluation",
     "evaluate_holdout_predictions", "global_strategy_oracle",
     "graph_oracle", "routing_summary", "topology_vector",
-    "summarize_generalization", "summarize_routing_folds",
+    "summarize_generalization", "summarize_routing_folds", "summarize_transfer_folds",
     "GraphDataset", "standard_reference_corpus",
     "SnapDataset", "download_snap_dataset", "snap_reference_corpus", "snap_scalability_corpus",
     "bootstrap_mean_ci", "graph_metric_means",
