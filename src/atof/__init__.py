68a459a3011dc045375e2397e1972e3d8c90c6a6
Chris Hotza <chrishotzaoficial@gmail.com>
2026-09-23 19:57:52 -0300

    Export regime trajectory and counterfactual research primitives

--- src/atof/__init__.py
+++ src/atof/__init__.py
@@ -6,6 +6,20 @@ from .partition import balance_error, edge_cut, initialize_balanced_partition, w
 from .strategies import BLOCReloc, PartitionResult
 from .topology import TopologyProfiler, TopologyProfile
 from .selector import HeuristicRegimeSelector, StrategyRecommendation
+from .adaptive import (
+    ControllerDecision,
+    PortfolioControllerV2,
+    RegimeSignatureV2,
+    TrajectoryMonitor,
+    TrajectoryState,
+    adaptive_regime_summary,
+    build_regime_signature_v2,
+)
+from .counterfactual import (
+    CounterfactualBenchmarkResult,
+    CounterfactualFold,
+    leave_one_family_out,
+)
 from .snap import SnapDataset, download_snap_dataset, snap_reference_corpus, snap_scalability_corpus
 from .routing import (
     LearnedTopologyRouter,
@@ -40,9 +54,13 @@ __all__ = [
     "BLOCReloc", "PartitionResult",
     "TopologyProfiler", "TopologyProfile",
     "HeuristicRegimeSelector", "StrategyRecommendation",
+    "RegimeSignatureV2", "build_regime_signature_v2", "adaptive_regime_summary",
+    "TrajectoryMonitor", "TrajectoryState",
+    "PortfolioControllerV2", "ControllerDecision",
     "LearnedTopologyRouter", "NearestTopologyRouter", "RoutingEvaluation",
     "evaluate_holdout_predictions", "global_strategy_oracle",
     "graph_oracle", "routing_summary", "topology_vector",
+    "CounterfactualBenchmarkResult", "CounterfactualFold", "leave_one_family_out",
     "summarize_generalization", "summarize_routing_folds", "summarize_transfer_folds",
     "GraphDataset", "standard_reference_corpus",
     "SnapDataset", "download_snap_dataset", "snap_reference_corpus", "snap_scalability_corpus",
